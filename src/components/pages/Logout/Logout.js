a4e0da8aa713365dd7ea2dbe76df1d66aabad4f9
Sena Akbulut <Senakbulut@users.noreply.github.com>
2021-05-09 16:33:47 +0300

    popup add

--- src/components/pages/Logout/Logout.js
+++ src/components/pages/Logout/Logout.js
@@ -2,7 +2,8 @@ import React,{useCallback,useContext,useState} from 'react';
 import { withRouter, Redirect } from "react-router";
 import { Link} from 'react-router-dom';
 import axios from 'axios'
-import Popup from 'reactjs-popup';
+import './Logout.css'
+import { useTranslation } from 'react-i18next';
 
 
 export default function Logout() {
@@ -29,19 +30,43 @@ console.log("result", result)
 
 
 
+const [modalState, setModalState] = useState(true)
 
-    return (
-        <div>
-           <Link to = "/">
-               <button
-                onClick={logout}
-               >
-                   Logout
+const toggleModalState = () => {
+  setModalState(!modalState)
+}
+const {t, i18n} = useTranslation();
 
-               </button>
-           </Link>
-           
+return (
+  <div className="App">
+    <div className={`modalBackground modalShowing-${modalState}`}>
+      <div className="modalInner">
+        <div className="modalImage">
+          <img
+            src="images/earthsad.gif"
+            alt="modal pic"
+          />
         </div>
+        <div className="modalText">
+          <h2>{t('Popup.header')}</h2>
+          <p>
+          {t('Popup.text1')}
+          <br/><br/> {t('Popup.text2')}
+          </p>
+          <form action="">
+            <button className="outbtn" onClick={logout}>{t('Popup.logout')}</button>
+          </form>
+          <button className="exitButton">
+              <Link to="/">
+              <span>{t('Popup.exit')}</span> 
+              </Link>
+            
+          </button>
+        </div>
+      </div>
+    </div>
+    {/* <button onClick={() => toggleModalState()}>Open modal</button> */}
+  </div>
     )
 }
 
