be85ae06dfa1093d6e8e57bc38663ffc99a85612
Sena Akbulut <Senakbulut@users.noreply.github.com>
2021-05-12 18:30:38 +0300

    imageUpload working

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -1,28 +1,71 @@
-import React, { Component } from 'react'
+import React,{ useState }  from 'react'
 import {MdPhotoLibrary} from 'react-icons/md'
 import {IconContext} from 'react-icons/lib'
 import {RiFolderUploadFill} from 'react-icons/ri'
 import './ImageUpload.css'
+import {storage} from '../components/firebase'
 
-
-export default class ImageUpload extends Component {
-    render() {
-        return (
-            <div >
-                 <IconContext.Provider value={{color:'#58c4bc'}}>
-                <br/>
-                <img src='https://ardeco.com.tr/public/upload/20200306095606_profilpng.png' alt="" height="100" width="500" />
-                <br/>
-                <br/>  
-                   <div className="kapsayici">
-                   <input type="file" id="file" onChange={this.handleChange} hidden/> 
-                   <label htmlFor="file" id="selector" >
-                       <MdPhotoLibrary /> &nbsp;Select File</label>
-                  <button className="buttons" onClick={this.handleUpload} align="center" >
-                      <RiFolderUploadFill/> &nbsp;Upload File</button> 
-                   </div>
-                   </IconContext.Provider>
-            </div>
-        )
-    }
+const ImageUpload=()=>{
+    const [image, setImage] = useState(null);
+    const [url, setUrl] = useState("");
+    const [progress, setProgress] = useState(0);
+  
+    const handleChange = e => {
+      if (e.target.files[0]) {
+        setImage(e.target.files[0]);
+       
+      }
+    };
+  
+    const handleUpload = () => {
+      const uploadTask = storage.ref(`images/${image.name}`).put(image);
+      uploadTask.on(
+        "state_changed",
+        snapshot => {
+          const progress = Math.round(
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+          setProgress(progress);
+        },
+        error => {
+          console.log(error);
+        },
+        () => {
+          storage
+            .ref("images").child(image.name).getDownloadURL()
+            .then(url => {
+              setUrl(url);
+            localStorage.setItem('imageUrl',url.toString());
+            window.location.reload();
+            });
+     
+           
+            
+        }
+      );
+    };
+  
+    return (
+        <div >
+             <IconContext.Provider value={{color:'#58c4bc'}}>
+            <br/>
+            <img src={ localStorage.getItem('imageUrl') == null ? "https://ardeco.com.tr/public/upload/20200306095606_profilpng.png" : localStorage.getItem('imageUrl') } alt="firebase-image" />
+            
+            <br/>
+           
+            <br/>  
+               <div className="kapsayici">
+               <input type="file" id="file" onChange={handleChange} hidden/> 
+               <label htmlFor="file" id="selector" >
+                   <MdPhotoLibrary /> &nbsp;Select File</label>
+              <button className="buttons" onClick={handleUpload} align="center" >
+                  <RiFolderUploadFill/> &nbsp;Upload File</button> 
+               </div>
+               </IconContext.Provider>
+        </div>
+    )
 }
+ 
+export default ImageUpload
+        
+    
+
