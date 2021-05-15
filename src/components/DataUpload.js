100aa95cad211ac690b1f7c3f3f580d926c9f115
Sena Akbulut <Senakbulut@users.noreply.github.com>
2021-05-15 15:04:34 +0300

    user data is pulled according to login status

--- src/components/DataUpload.js
+++ src/components/DataUpload.js
@@ -2,34 +2,33 @@
  import { useTranslation } from 'react-i18next';
 import './DataUpload.css'
 function DataUpload() {
-//     constructor(){
-//         super();
-//         this.state = {
-//            name:"sena"
-//         };
-//     }
-//      componentDidMount(){
-//         const rootRef = firebase.database().ref().child('react');
-//         const nameRef = rootRef.child('name');
-//         nameRef.on('value',snap =>{
-//          this.setState({
-//              name:snap.val()
-//          })
-//         })
-//      }
+const {t, i18n} = useTranslation();
+
 const [data, setData] = useState([]);
+const [Namedata, setNamedata]= useState([]);
 useEffect(()=>{
     (async ()=>{
         const respData = await fetch("http://localhost:5000/users")
         const jsonData = await respData.json()
-        const itemData = jsonData.data[1]
+        const [itemData] = jsonData.data
         setData(itemData);
+
+        for(let i=0;i < (jsonData.count); i++){
+            if(jsonData.data[i].login == true){
+             var idUserData=jsonData.data[i]._id
+              const resData = await fetch(`http://localhost:5000/users/${idUserData}`)
+              const jsoData = await resData.json()
+              setNamedata(jsoData.data.name); 
+              
+            }
+           
+          }
     })();
 }, []);
-const {t, i18n} = useTranslation();
+
          return (
              <div>
-                 <h3 className="Username">{t('Account.header')} {data.name}</h3>
+                 <h3 className="Username">{t('Account.header')} {Namedata}</h3>
                  
              </div>
          )
